3301e36aad0fc020699f9deb56de28bef42467fd
Lucas <lucas@dominiol.com.br>
2015-08-23 19:59:24 -0300

    Finalização de testes para descrições. Falta ainda verificar a questão da suite vazia.

--- java/br/ufsc/ine/leb/projetos/estoria/testes/TesteEscoltadorDeTestesDescricoes.java
+++ java/br/ufsc/ine/leb/projetos/estoria/testes/TesteEscoltadorDeTestesDescricoes.java
@@ -13,7 +13,7 @@ import br.ufsc.ine.leb.projetos.estoria.SeletorDeTestes;
 import br.ufsc.ine.leb.projetos.estoria.testes.figuracao.classes.ClasseComDoisMetodosDeTestePassando;
 import br.ufsc.ine.leb.projetos.estoria.testes.figuracao.classes.ClasseComUmMetodoDeTestePassando;
 
-public final class TesteEscoltadorDeTestes {
+public final class TesteEscoltadorDeTestesDescricoes {
 
 	@Test
 	public void semTestesJunit() throws Exception {
@@ -107,6 +107,54 @@ public final class TesteEscoltadorDeTestes {
 		assertFalse(descricaoDoTeste2.isEmpty());
 		assertEquals(0, descricaoDoTeste2.getChildren().size());
 		assertEquals(1, descricaoDoTeste2.testCount());
+	}
+
+	@Test
+	public void tresTestesDeDuasClasses() throws Exception {
+		SeletorDeTestes seletor = new SeletorDeTestes();
+		seletor.adicionarClasse(ClasseComUmMetodoDeTestePassando.class);
+		seletor.adicionarClasse(ClasseComDoisMetodosDeTestePassando.class);
+		EscoltadorDeTestes escoltador = new EscoltadorDeTestes(seletor);
+
+		Description descricaoDoSeletor = escoltador.getDescription();
+		assertEquals(seletor.getClass(), descricaoDoSeletor.getTestClass());
+		assertEquals(seletor.getClass().getName(), descricaoDoSeletor.getClassName());
+		assertEquals(null, descricaoDoSeletor.getMethodName());
+		assertTrue(descricaoDoSeletor.isSuite());
+		assertFalse(descricaoDoSeletor.isTest());
+		assertFalse(descricaoDoSeletor.isEmpty());
+		assertEquals(3, descricaoDoSeletor.getChildren().size());
+		assertEquals(3, descricaoDoSeletor.testCount());
+
+		Description descricaoDoTeste1 = descricaoDoSeletor.getChildren().get(0);
+		assertEquals(ClasseComUmMetodoDeTestePassando.class, descricaoDoTeste1.getTestClass());
+		assertEquals(ClasseComUmMetodoDeTestePassando.class.getName(), descricaoDoTeste1.getClassName());
+		assertEquals("testar", descricaoDoTeste1.getMethodName());
+		assertFalse(descricaoDoTeste1.isSuite());
+		assertTrue(descricaoDoTeste1.isTest());
+		assertFalse(descricaoDoTeste1.isEmpty());
+		assertEquals(0, descricaoDoTeste1.getChildren().size());
+		assertEquals(1, descricaoDoTeste1.testCount());
+
+		Description descricaoDoTeste2 = descricaoDoSeletor.getChildren().get(1);
+		assertEquals(ClasseComDoisMetodosDeTestePassando.class, descricaoDoTeste2.getTestClass());
+		assertEquals(ClasseComDoisMetodosDeTestePassando.class.getName(), descricaoDoTeste2.getClassName());
+		assertEquals("testar1", descricaoDoTeste2.getMethodName());
+		assertFalse(descricaoDoTeste2.isSuite());
+		assertTrue(descricaoDoTeste2.isTest());
+		assertFalse(descricaoDoTeste2.isEmpty());
+		assertEquals(0, descricaoDoTeste2.getChildren().size());
+		assertEquals(1, descricaoDoTeste2.testCount());
+
+		Description descricaoDoTeste3 = descricaoDoSeletor.getChildren().get(2);
+		assertEquals(ClasseComDoisMetodosDeTestePassando.class, descricaoDoTeste3.getTestClass());
+		assertEquals(ClasseComDoisMetodosDeTestePassando.class.getName(), descricaoDoTeste3.getClassName());
+		assertEquals("testar2", descricaoDoTeste3.getMethodName());
+		assertFalse(descricaoDoTeste3.isSuite());
+		assertTrue(descricaoDoTeste3.isTest());
+		assertFalse(descricaoDoTeste3.isEmpty());
+		assertEquals(0, descricaoDoTeste3.getChildren().size());
+		assertEquals(1, descricaoDoTeste3.testCount());
 	}
 
 }
