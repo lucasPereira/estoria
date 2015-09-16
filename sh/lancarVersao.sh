183258eb0ff1cba6b6a2252f5c5deabb42232d42
Lucas <lucas@dominiol.com.br>
2015-09-16 03:28:08 -0300

    Ajuste no script de lançamento de versão.

--- sh/lancarVersao.sh
+++ sh/lancarVersao.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
 versao='1.1'
+
+caminho=`pwd`
 cd ..
+
 git add .
-git commit -m "Lançando versão ${versao}"
+git commit -m "Lançando versão ${versao}."
 git tag -a ${versao} -m "Estória ${versao}"
+
+cd ${caminho}
